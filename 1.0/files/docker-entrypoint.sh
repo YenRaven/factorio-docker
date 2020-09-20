ad82e16aedeee2d4868629027497f4b5e1559d84
YenRaven <YenRaven@gmail.com>
2020-09-20 04:02:54 -0500

    Update docker-entrypoint.sh

--- 1.0/files/docker-entrypoint.sh
+++ 1.0/files/docker-entrypoint.sh
@@ -21,16 +21,16 @@ fi
 if [[ ! -f $CONFIG/server-settings.json ]]; then
   # Copy default settings if server-settings.json doesn't exist
   cp /opt/factorio/data/server-settings.example.json "$CONFIG/server-settings.json"
-  if [[ -z ${USERNAME:-}]]; then
-    sed -i '/"username":/c\"username":"${USERNAME:-}",/' "$CONFIG/server-settings.json"
-    sed -i '/"token":/c\"token":"${TOKEN:-}",/' "$CONFIG/server-settings.json"
-  fi
-  if [[ -z ${GAME_NAME:-}]]; then
-    sed -i '/"name":/c\"name":"${GAME_NAME:-}",/' "$CONFIG/server-settings.json"
-  fi
-  if [[ -z ${GAME_DESC:-}]]; then
-    sed -i '/"description":/c\"description":"${GAME_DESC:-}",/' "$CONFIG/server-settings.json"
-  fi
+  #if [[ -z ${USERNAME:-}]]; then
+  #  sed -i '/"username":/c\"username":"${USERNAME:-}",/' "$CONFIG/server-settings.json"
+  #  sed -i '/"token":/c\"token":"${TOKEN:-}",/' "$CONFIG/server-settings.json"
+  #fi
+  #if [[ -z ${GAME_NAME:-}]]; then
+  #  sed -i '/"name":/c\"name":"${GAME_NAME:-}",/' "$CONFIG/server-settings.json"
+  #fi
+  #if [[ -z ${GAME_DESC:-}]]; then
+  #  sed -i '/"description":/c\"description":"${GAME_DESC:-}",/' "$CONFIG/server-settings.json"
+  #fi
 fi
 
 if [[ ! -f $CONFIG/map-gen-settings.json ]]; then
